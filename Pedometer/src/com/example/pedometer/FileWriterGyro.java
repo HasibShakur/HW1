3f0f28d951c2aa492ec8c08d39079ccc2225c701
wyatt_000 <wyatt_000@Wyatt-Acer>
2013-10-14 09:45:43 -0500

    Small Changes -- Code cleanup

--- Pedometer/src/com/example/pedometer/FileWriterGyro.java
+++ Pedometer/src/com/example/pedometer/FileWriterGyro.java
@@ -1,32 +1,23 @@
 package com.example.pedometer;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Writer;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 import android.os.Environment;
-import android.util.Log;
-
-import com.example.pedometer.AccData;
 
 class AsyncFileWriterGyro implements Runnable {
-    private final File file;
     private final PrintWriter out;
     private final BlockingQueue<GyroData> queue = new LinkedBlockingQueue<GyroData>();
     private volatile boolean started = false;
     private volatile boolean stopped = false;
 
     public AsyncFileWriterGyro() throws IOException {
-        File f = new File(Environment.getExternalStorageDirectory() + File.separator + "trace1-data-gyro.csv");
-        this.file = f;
-		this.out = new PrintWriter(new FileOutputStream(f), true);
+        File f = new File(Environment.getExternalStorageDirectory() + File.separator + "trace-data-gyro.csv");
+        this.out = new PrintWriter(new FileOutputStream(f), true);
     }
     
     
